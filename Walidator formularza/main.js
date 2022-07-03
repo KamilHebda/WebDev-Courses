47ace32abfd4ae5a53332809f8e3b0cfa1d32a28
Kamil <46973027+KamilHebda@users.noreply.github.com>
2022-07-03 16:26:39 +0200

    Add files via upload

--- Walidator formularza/main.js
+++ Walidator formularza/main.js
@@ -53,6 +53,32 @@ const checkPassword = (pass1, pass2) => {
   }
 };
 
+const checkMail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(email.value)) {
+    clearError(email);
+  } else {
+    showError(email, "Błędny adres e-mail");
+  }
+};
+
+const checkErrors = () => {
+  const allInputs = document.querySelectorAll(".form-box");
+  let errorCount = 0;
+
+  allInputs.forEach((el) => {
+    if (el.classList.contains("error")) {
+      errorCount++;
+    }
+  });
+
+  if (errorCount === 0) {
+    popup.classList.add("show-popup");
+  }
+};
+
 sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -60,6 +86,8 @@ sendBtn.addEventListener("click", (e) => {
   checkLength(username, 3);
   checkLength(pass, 8);
   checkPassword(pass, pass2);
+  checkMail(email);
+  checkErrors();
 });
 
 clearBtn.addEventListener("click", (e) => {
@@ -67,5 +95,6 @@ clearBtn.addEventListener("click", (e) => {
 
   [username, pass, pass2, email].forEach((el) => {
     el.value = "";
+    clearError(el);
   });
 });
